7fea621048b892aae492dbaf5e564fb9c2d090d6
welenmota75 <71537729+welenmota75@users.noreply.github.com>
2023-06-05 02:51:54 -0300

    Add files via upload

---
+++ projeto de java2/Juntos-Somos-Mais/src/main/java/br/edu/ifsp/spo/lp2a4/Juntos/Somos/Mais/JuntosSomosMaisApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.lp2a4.Juntos.Somos.Mais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuntosSomosMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuntosSomosMaisApplication.class, args);
+	}
+
+}
---
+++ projeto de java2/Juntos-Somos-Mais/src/test/java/br/edu/ifsp/spo/lp2a4/Juntos/Somos/Mais/JuntosSomosMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.lp2a4.Juntos.Somos.Mais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuntosSomosMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
