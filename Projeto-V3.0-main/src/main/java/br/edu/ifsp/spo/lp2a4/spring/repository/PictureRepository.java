eecc3ca188c63dcdaa674498e122de7cea88215e
welenmota75 <71537729+welenmota75@users.noreply.github.com>
2023-06-12 16:02:56 -0300

    Delete Projeto-V3.0-main directory

--- Projeto-V3.0-main/src/main/java/br/edu/ifsp/spo/lp2a4/spring/repository/PictureRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifsp.spo.lp2a4.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.edu.ifsp.spo.lp2a4.spring.model.Picture;
-
-public interface PictureRepository extends CrudRepository<Picture, Integer>{
-
-}
